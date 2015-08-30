cb4d078ee76bb2fdbf3c27455a974417785b450a
Mariano Ruiz <mrsarm@gmail.com>
2015-08-30 16:19:07 -0300

    Fix merge Unknown operation "killcursors" patch

--- mongotail/out.py
+++ mongotail/out.py
@@ -52,10 +52,9 @@ def print_obj(obj):
                 query += '%s inserted.' % obj['ninserted']
             elif operation == 'remove':
                 query += '. %s deleted.' % obj['ndeleted']
-                query += '. %s deleted.' % obj['ndeleted']
-	    elif operation == "killcursors":
-	        operation = "killcursors"
-	        query = None
+        elif operation == "killcursors":
+            operation = "killcursors"
+            query = None
         elif operation == "command":
             if 'count' in obj["command"]:
                 operation = "count"
